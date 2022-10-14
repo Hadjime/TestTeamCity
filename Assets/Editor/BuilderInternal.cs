d9f5d6f6aa47650ccf15ee96e84f2dfc1b130e1d
Roma_G <Gavrilov@inbox.ru>
2022-10-14 18:35:44 +0700

    - addition package for build

--- Assets/Editor/BuilderInternal.cs
+++ Assets/Editor/BuilderInternal.cs
@@ -9,67 +9,67 @@ namespace Editor
 {
     public class BuilderInternal : MonoBehaviour
     {
-        [MenuItem("Build/BuildAndroidProject")]
-        public static void BuildAndroidProject()
-        {
-            var folderName = "BuildAndroid";
-            DeleteFolder (folderName);
-            CreateFolder (folderName);
-            string[] scenes = GetScenes();
-            BuildReport error = BuildPipeline.BuildPlayer(scenes, folderName, BuildTarget.Android, BuildOptions.None);
-            if (error != null)
-            {
-                throw new Exception("Build failed: " + error);
-            }
-        }
-        
-        [MenuItem("Build/BuildApk")]
-        public static void BuildApk()
-        {
-            var outdir = System.Environment.CurrentDirectory + "/BuildOutPutPath/Android";
-            var outputPath = Path.Combine(outdir, Application.productName + ".apk");
-            // Обработка папки
-            if (!Directory.Exists(outdir)) Directory.CreateDirectory(outdir);
-            if (File.Exists(outputPath)) File.Delete(outputPath);
- 
-            // Запускаем проект в один клик
-            string[] scenes = GetScenes();
-            UnityEditor.BuildPipeline.BuildPlayer(scenes, outputPath, BuildTarget.Android, BuildOptions.None);
-            if (File.Exists(outputPath))
-            {
-                Debug.Log("Build Success :" + outputPath);
-            }
-            else
-            {
-                Debug.LogException(new Exception("Build Fail! Please Check the log! "));
-            }
-        }
-
-        private static void CreateFolder(string name)
-        {
-            if (!Directory.Exists(name))
-            {
-                Directory.CreateDirectory(name);
-            }
-        }
-
-        private static void DeleteFolder(string name)
-        {
-            if (Directory.Exists(name))
-            {
-                Directory.Delete(name, true);
-            }
-        }
-
-        private static string[] GetScenes()
-        {
-            string[] scenes = new string[EditorBuildSettings.scenes.Length];
-            for(int i = 0; i < scenes.Length; i++)
-            {
-                scenes[i] = EditorBuildSettings.scenes[i].path;
-            }
-            return scenes;
-        }
+        // [MenuItem("Build/BuildAndroidProject")]
+        // public static void BuildAndroidProject()
+        // {
+        //     var folderName = "BuildAndroid";
+        //     DeleteFolder (folderName);
+        //     CreateFolder (folderName);
+        //     string[] scenes = GetScenes();
+        //     BuildReport error = BuildPipeline.BuildPlayer(scenes, folderName, BuildTarget.Android, BuildOptions.None);
+        //     if (error != null)
+        //     {
+        //         throw new Exception("Build failed: " + error);
+        //     }
+        // }
+        //
+        // [MenuItem("Build/BuildApk")]
+        // public static void BuildApk()
+        // {
+        //     var outdir = System.Environment.CurrentDirectory + "/BuildOutPutPath/Android";
+        //     var outputPath = Path.Combine(outdir, Application.productName + ".apk");
+        //     // Обработка папки
+        //     if (!Directory.Exists(outdir)) Directory.CreateDirectory(outdir);
+        //     if (File.Exists(outputPath)) File.Delete(outputPath);
+        //
+        //     // Запускаем проект в один клик
+        //     string[] scenes = GetScenes();
+        //     UnityEditor.BuildPipeline.BuildPlayer(scenes, outputPath, BuildTarget.Android, BuildOptions.None);
+        //     if (File.Exists(outputPath))
+        //     {
+        //         Debug.Log("Build Success :" + outputPath);
+        //     }
+        //     else
+        //     {
+        //         Debug.LogException(new Exception("Build Fail! Please Check the log! "));
+        //     }
+        // }
+        //
+        // private static void CreateFolder(string name)
+        // {
+        //     if (!Directory.Exists(name))
+        //     {
+        //         Directory.CreateDirectory(name);
+        //     }
+        // }
+        //
+        // private static void DeleteFolder(string name)
+        // {
+        //     if (Directory.Exists(name))
+        //     {
+        //         Directory.Delete(name, true);
+        //     }
+        // }
+        //
+        // private static string[] GetScenes()
+        // {
+        //     string[] scenes = new string[EditorBuildSettings.scenes.Length];
+        //     for(int i = 0; i < scenes.Length; i++)
+        //     {
+        //         scenes[i] = EditorBuildSettings.scenes[i].path;
+        //     }
+        //     return scenes;
+        // }
     }
 }
 #endif
